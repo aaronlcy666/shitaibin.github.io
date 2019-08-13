638c72b2f8b9728362d544ab0af0e77cf90c7824
Shitaibin <hz_stb@163.com>
2019-08-13 16:53:09 +0800

    auto backup

--- deploy_and_backup_hexo_br.sh
+++ deploy_and_backup_hexo_br.sh
@@ -1,7 +1,12 @@
 python2 md_image_backup.py
+echo "===================== Image backup success ====================="
 hexo clean
 git add .
 git commit -m 'auto backup'
+echo "===================== Local commit success ====================="
 git push origin hexo_resource
+echo "===================== Push to github success ====================="
 git push coding hexo_resource
+echo "===================== Push to tencent success ====================="
 hexo d
+echo "===================== Deploy to github and tencent success ====================="
