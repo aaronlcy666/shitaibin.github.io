d438ec1147ad0eac12b4eca196b77817f8d4cecc
Shitaibin <hz_stb@163.com>
2020-03-01 16:16:23 +0800

    auto backup

--- deploy_and_backup_hexo_br.sh
+++ deploy_and_backup_hexo_br.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+#
+# source codes upload to github
+# website files upload to qiniu
+
 python2 md_image_backup.py
 echo "===================== Image backup success ====================="
+
 hexo clean
+
 git add .
 git commit -m 'auto backup'
 echo "===================== Local commit success ====================="
+
 git push origin hexo_resource
-echo "===================== Push to github success ====================="
-git push coding hexo_resource
-echo "===================== Push to tencent success ====================="
-hexo d
-echo "===================== Deploy to github and tencent success ====================="
+echo "===================== Push resource to github success ====================="
+
+echo "===================== Generate website files ====================="
+hexo generate
+
+echo "===================== Push website files to Qiniu ====================="
+qshell qupload qiniu.conf
+
+# No need deploy to github
+# hexo d
+# echo "===================== Deploy to github success ====================="
